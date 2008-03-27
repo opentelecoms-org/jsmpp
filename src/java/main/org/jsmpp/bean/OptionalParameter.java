d4d059e20547525d2aa04c7a32a7758da76f2077
janne.hietamaki <janne.hietamaki@1678c8a9-8d33-0410-b0a4-d546c816ed44>
2008-03-27 12:55:24 +0000

    fix formatting, print optional parameter codes as hex

--- src/java/main/org/jsmpp/bean/OptionalParameter.java
+++ src/java/main/org/jsmpp/bean/OptionalParameter.java
@@ -2,6 +2,7 @@ package org.jsmpp.bean;
 
 import java.nio.ByteBuffer;
 
+import org.jsmpp.util.IntUtil;
 import org.jsmpp.util.OctetUtil;
 
 public abstract class OptionalParameter {
@@ -157,7 +158,50 @@ public abstract class OptionalParameter {
     }
 
     public enum Tag {
-        dest_addr_subunit(0x0005, Byte.class), dest_network_type(0x0006, Byte.class), dest_bearer_type(0x0007, Byte.class), dest_telematics_id(0x0008, Short.class), source_addr_subunit(0x000D, Byte.class), source_network_type(0x000E, Byte.class), source_bearer_type(0x000F, Byte.class), source_telematics_id(0x0010, Byte.class), qos_time_to_live(0x0017, Int.class), payload_type(0x0019, Byte.class), additional_status_info_text(0x001D, COctetString.class), receipted_message_id(0x001E, COctetString.class), ms_msg_wait_facilities(0x0030, Byte.class), privacy_indicator(0x0201, Byte.class), source_subaddress(0x0202, OctetString.class), dest_subaddress(0x0203, OctetString.class), user_message_reference(0x0204, Short.class), user_response_code(0x0205, Byte.class), source_port(0x020A, Short.class), destination_port(0x020B, Short.class), sar_msg_ref_num(0x020C, Short.class), language_indicator(0x020D, Byte.class), sar_total_segments(0x020E, Byte.class), sar_segment_seqnum(0x020F, Byte.class), SC_interface_version(0x0210, Byte.class), callback_num_pres_ind(0x0302, Byte.class), callback_num_atag(0x0303, OctetString.class), number_of_messages(0x0304, Byte.class), callback_num(0x0381, OctetString.class), dpf_result(0x0420, Byte.class), set_dpf(0x0421, Byte.class), ms_availability_status(0x0422, Byte.class), network_error_code(0x0423, OctetString.class), message_payload(0x0424, OctetString.class), delivery_failure_reason(0x0425, Byte.class), more_messages_to_send(0x0426, Byte.class), message_state(0x0427, Byte.class), ussd_service_op(0x0501, Byte.class), display_time(0x1201, Byte.class), sms_signal(0x1203, Short.class), ms_validity(0x1204, Byte.class), alert_on_message_delivery(0x130C, Null.class), its_reply_type(0x1380, Byte.class), its_session_info(0x1383, Short.class);
+        dest_addr_subunit(0x0005, Byte.class), 
+        dest_network_type(0x0006, Byte.class), 
+        dest_bearer_type(0x0007, Byte.class),
+        dest_telematics_id(0x0008, Short.class)
+        , source_addr_subunit(0x000D, Byte.class), 
+        source_network_type(0x000E, Byte.class),
+        source_bearer_type(0x000F, Byte.class), 
+        source_telematics_id(0x0010, Byte.class), 
+        qos_time_to_live(0x0017, Int.class),
+        payload_type(0x0019, Byte.class), 
+        additional_status_info_text(0x001D, COctetString.class),
+        receipted_message_id(0x001E, COctetString.class),
+        ms_msg_wait_facilities(0x0030, Byte.class),
+        privacy_indicator(0x0201, Byte.class), 
+        source_subaddress(0x0202, OctetString.class),
+        dest_subaddress(0x0203, OctetString.class),
+        user_message_reference(0x0204, Short.class), 
+        user_response_code(0x0205, Byte.class), 
+        source_port(0x020A, Short.class),
+        destination_port(0x020B, Short.class), 
+        sar_msg_ref_num(0x020C, Short.class), 
+        language_indicator(0x020D, Byte.class),
+        sar_total_segments(0x020E, Byte.class), 
+        sar_segment_seqnum(0x020F, Byte.class), 
+        SC_interface_version(0x0210, Byte.class),
+        callback_num_pres_ind(0x0302, Byte.class),
+        callback_num_atag(0x0303, OctetString.class),
+        number_of_messages(0x0304, Byte.class), 
+        callback_num(0x0381, OctetString.class), 
+        dpf_result(0x0420, Byte.class), 
+        set_dpf(0x0421, Byte.class), 
+        ms_availability_status(0x0422, Byte.class), 
+        network_error_code(0x0423, OctetString.class),
+        message_payload(0x0424, OctetString.class), 
+        delivery_failure_reason(0x0425, Byte.class), 
+        more_messages_to_send(0x0426, Byte.class),
+        message_state(0x0427, Byte.class),
+        ussd_service_op(0x0501, Byte.class), 
+        display_time(0x1201, Byte.class),
+        sms_signal(0x1203, Short.class), 
+        ms_validity(0x1204, Byte.class),
+        lert_on_message_delivery(0x130C, Null.class), 
+        its_reply_type(0x1380, Byte.class),
+        its_session_info(0x1383, Short.class);
 
         private final short value;
         private final Class<? extends OptionalParameter> type;
@@ -176,7 +220,7 @@ public abstract class OptionalParameter {
                 if (tag.value() == code)
                     return tag;
             }
-            throw new IllegalArgumentException("No tag for: " + code);
+            throw new IllegalArgumentException("No tag for: " + IntUtil.toHexString(code));
         }
     }
 }
