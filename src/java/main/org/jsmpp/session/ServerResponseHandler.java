05fc96172c6cdd5dc4f0d73fd7ce1bc31a16fb5d
janne.hietamaki <janne.hietamaki@1678c8a9-8d33-0410-b0a4-d546c816ed44>
2008-04-09 16:01:20 +0000

    make ServerMessageReceiverListener nullable

--- src/java/main/org/jsmpp/session/ServerResponseHandler.java
+++ src/java/main/org/jsmpp/session/ServerResponseHandler.java
@@ -5,19 +5,32 @@ import org.jsmpp.bean.QuerySm;
 import org.jsmpp.bean.SubmitSm;
 import org.jsmpp.extra.ProcessRequestException;
 import org.jsmpp.util.MessageId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServerResponseHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ServerResponseHandler.class);
 
     public MessageId processSubmitSm(ServerSession session, SubmitSm submitSm) throws ProcessRequestException {
-        MessageId messageId = session.getMessageReceiverListener().onAcceptSubmitSm(submitSm);
-        session.getPDUSender().sendSubmitSmResp(submitSm, messageId);
-        return messageId;
+        ServerMessageReceiverListener messageReceiverListener = session.getMessageReceiverListener();
+        if (messageReceiverListener != null) {
+            MessageId messageId = messageReceiverListener.onAcceptSubmitSm(submitSm);
+            session.getPDUSender().sendSubmitSmResp(submitSm, messageId);
+            return messageId;
+        }
+        logger.debug("ServerResponseHandler received submit_sm, but ServerMessageReceiverListener is not set");
+        return null;
     }
 
     public QuerySmResult processQuerySm(ServerSession session, QuerySm querySm) throws ProcessRequestException {
-        QuerySmResult res = session.getMessageReceiverListener().onAcceptQuerySm(querySm);
-        session.getPDUSender().sendQuerySmResp(querySm, res);
-        return res;
+        ServerMessageReceiverListener messageReceiverListener = session.getMessageReceiverListener();
+        if (messageReceiverListener != null) {
+            QuerySmResult res = messageReceiverListener.onAcceptQuerySm(querySm);
+            session.getPDUSender().sendQuerySmResp(querySm, res);
+            return res;
+        }
+        logger.debug("ServerResponseHandler received query_sm, but ServerMessageReceiverListener is not set");
+        return null;
     }
 
     public void processBind(ServerSession session, Bind bind) {
