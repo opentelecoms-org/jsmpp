60a6724488e557a4865439a00506555e1f15987e
uudashr <uudashr@1678c8a9-8d33-0410-b0a4-d546c816ed44>
2008-04-14 09:54:50 +0000

    Clean up repository in order to re-assembly the design

--- src/java/main/org/jsmpp/session/state/States.java
+++
@@ -1,7 +0,0 @@
-package org.jsmpp.session.state;
-
-import org.jsmpp.session.Session;
-
-public interface States<T extends SessionState, V extends Session<T>> {
-    public T sessionStateForState(V session, State m);
-}
