a9dd555e0ac4f632104cb1ab6a6cf049e9d8defa
uudashr <uudashr@1678c8a9-8d33-0410-b0a4-d546c816ed44>
2008-05-26 02:48:09 +0000

    Fix and complete the features

--- src/java/main/org/jsmpp/bean/GSMSpecificFeature.java
+++ src/java/main/org/jsmpp/bean/GSMSpecificFeature.java
@@ -5,13 +5,22 @@ package org.jsmpp.bean;
  * 
  */
 public enum GSMSpecificFeature {
-
-    // TODO uudashr: complete the specific feature
-    DEFAULT((byte) 0x00), 
+    DEFAULT((byte) 0x00),
+    
     /**
      * User data header indicator.
      */
-    UDHI((byte) 0xc0);
+    UDHI((byte)0x40), 
+    
+    /**
+     * Reply path.
+     */
+    REPLYPATH((byte)0x80),
+    
+    /**
+     * User data header indicator and Reply path.
+     */
+    UDHI_REPLYPATH((byte) 0xc0);
 
     public static final byte CLEAR_BYTE = 0x3f;
     public static final byte MASK_BYTE = (byte) 0xc0;
@@ -26,19 +35,44 @@ public enum GSMSpecificFeature {
         return value;
     }
     
+    /**
+     * Check the GSM Specific Feature is contained in ESM class value.
+     * 
+     * @param esmClass is the ESM class.
+     * @return <tt>true</tt> if the GSM specific feature is contained in ESM class.
+     */
     public boolean containedIn(ESMClass esmClass) {
         return containedIn(esmClass.value());
     }
     
+    /**
+     * Check the GSM Specific Feature is contained in ESM class value.
+     * 
+     * @param esmClass is the ESM class.
+     * @return <tt>true</tt> if the GSM specific feature is contained in ESM class.
+     */
     public boolean containedIn(byte esmClass) {
-        return this.value == (byte)(esmClass & MASK_BYTE);
+        return this.value == clear(esmClass);
     }
     
+    /**
+     * Compose the existing ESM class with the GSM specific feature.
+     * 
+     * @param esmClass is the existing ESM class.
+     * @param specificFeature the GSM specific feature.
+     * @return the ESM class composed with specified GSM specific feature. 
+     */
     public static byte compose(byte esmClass, GSMSpecificFeature specificFeature) {
-        return (byte)(clean(esmClass) | specificFeature.value());
+        return (byte)(clear(esmClass) | specificFeature.value());
     }
     
-    public static byte clean(byte esmClass) {
-        return (byte) (esmClass & CLEAR_BYTE);
+    /**
+     * Clear the bit 0 - 6.
+     * 
+     * @param esmClass is the ESM class value.
+     * @return the masked value for ESM class.
+     */
+    public static byte clear(byte esmClass) {
+        return (byte)(esmClass & CLEAR_BYTE);
     }
 }
