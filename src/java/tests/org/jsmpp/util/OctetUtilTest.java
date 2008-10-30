2419124f3f57b491ff24a99b9bb804530cbcd6a8
uudashr <uudashr@1678c8a9-8d33-0410-b0a4-d546c816ed44>
2008-10-30 08:33:48 +0000

    Some octet util test

---
+++ src/java/tests/org/jsmpp/util/OctetUtilTest.java
@@ -0,0 +1,63 @@
+package org.jsmpp.util;
+
+import static org.testng.Assert.assertEquals;
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+import org.testng.annotations.Test;
+
+/**
+ * @author uudashr
+ *
+ */
+public class OctetUtilTest {
+    
+    /**
+     * Test the int2bytes and bytes2int conversion.
+     */
+    @Test
+    public void intConversion() {
+        int in = Integer.MIN_VALUE;
+        byte[] bytes = OctetUtil.intToBytes(in);
+        assertEquals(bytes.length, 4);
+        int out = OctetUtil.bytesToInt(bytes);
+        assertEquals(out, in);
+    }
+    
+    /**
+     * Test the short2bytes and bytes2short conversion.
+     */
+    @Test
+    public void shortConversion() {
+        short in = Short.MIN_VALUE;
+        byte[] bytes = OctetUtil.shortToBytes(in);
+        assertEquals(bytes.length, 2);
+        short out = OctetUtil.bytesToShort(bytes);
+        assertEquals(out, in);
+    }
+    
+    /**
+     * Test another short2bytes and bytes2short conversion. 
+     */
+    @Test
+    public void testAnotherShortConversion() {
+        byte[] bytes = new byte[] {-10, 10};
+        short shortVal = OctetUtil.bytesToShort(bytes);
+        byte[] anotherByte = OctetUtil.shortToBytes(shortVal);
+        assertEquals(anotherByte, bytes);
+        short anotherShortVal = OctetUtil.bytesToShort(anotherByte);
+        assertEquals(anotherShortVal, shortVal);
+    }
+}
