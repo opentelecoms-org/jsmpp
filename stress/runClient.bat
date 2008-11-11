a4d92e0244b884bb2aa117d9de01af9de7f04ac0
uudashr <uudashr@1678c8a9-8d33-0410-b0a4-d546c816ed44>
2008-11-11 09:08:13 +0000

    Fix the run script for stress test (for windows)

--- stress/runClient.bat
+++ stress/runClient.bat
@@ -8,9 +8,9 @@ cd %APP_HOME%
 
 SET LIB_DIR=lib\*.jar
 
-@FOR %%V IN (%LIB_DIR%) DO call "lcp.bat" %%~fV
+@FOR %%V IN (%LIB_DIR%) DO call "stress\lcp.bat" %%~fV
 
-@FOR %%V IN (*.jar) DO call "lcp.bat" %%~fV
+@FOR %%V IN (*.jar) DO call "stress\lcp.bat" %%~fV
 
 SET CLASSPATH=%LOCALCLASSPATH%
 echo %CLASSPATH%
