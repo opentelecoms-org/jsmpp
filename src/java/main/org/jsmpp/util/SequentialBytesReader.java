34f76d5706efc0a4620cdd66f68b385756d18201
uudashr <uudashr@1678c8a9-8d33-0410-b0a4-d546c816ed44>
2008-12-09 05:22:29 +0000

    Return empty byte if the given length is 0

--- src/java/main/org/jsmpp/util/SequentialBytesReader.java
+++ src/java/main/org/jsmpp/util/SequentialBytesReader.java
@@ -75,7 +75,7 @@ class SequentialBytesReader {
 
     public byte[] readBytes(int length) {
         if (length == 0)
-            return null;
+            return new byte[0];
         byte[] data = new byte[length];
         System.arraycopy(bytes, cursor, data, 0, length);
         cursor += length;
