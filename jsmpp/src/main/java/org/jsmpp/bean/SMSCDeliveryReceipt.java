c437717b9dca0f7fd247f1e1346eba0c27a4ada4
Pim Moerenhout <pim.moerenhout@gmail.com>
2016-04-14 14:11:01 +0200

    Fix

--- jsmpp/src/main/java/org/jsmpp/bean/SMSCDeliveryReceipt.java
+++ jsmpp/src/main/java/org/jsmpp/bean/SMSCDeliveryReceipt.java
@@ -35,7 +35,7 @@ public enum SMSCDeliveryReceipt {
      * SMSC Delivery Receipt requested where the final delivery outcome is
      * delivery failure.
      */
-    FAILURE((byte)0x02);
+    SUCCESS((byte)0x02);
 
     public static final byte CLEAR_BYTE = (byte)0xfc; // 11111100
     public static final byte MASK_BYTE = 0x03; // 00000011
