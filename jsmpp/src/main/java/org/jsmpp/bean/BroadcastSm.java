c5c30d87b813c3cfce3548644f3f9a9d757f9b23
Pim Moerenhout <pim.moerenhout@gmail.com>
2020-03-01 17:56:19 +0100

    Move to Java 7 and add SMPP 5.0 PDU objects.

---
+++ jsmpp/src/main/java/org/jsmpp/bean/BroadcastSm.java
@@ -0,0 +1,170 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.jsmpp.bean;
+
+import java.util.Arrays;
+
+import org.jsmpp.bean.OptionalParameter.Tag;
+
+/**
+ * @author pmoerenhout
+ * @since 3.0.0
+ */
+public class BroadcastSm extends Command {
+
+    private static final long serialVersionUID = -3167079324039268075L;
+
+    private String serviceType;
+    private byte sourceAddrTon;
+    private byte sourceAddrNpi;
+    private String sourceAddr;
+    private String messageId;
+    private byte priorityFlag;
+    private String scheduleDeliveryTime;
+    private String validityPeriod;
+    private byte replaceIfPresentFlag;
+    private byte dataCoding;
+    private byte smDefaultMsgId;
+    private OptionalParameter[] optionalParameters;
+
+    public BroadcastSm() {
+        super();
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public byte getSourceAddrTon() {
+        return sourceAddrTon;
+    }
+
+    public void setSourceAddrTon(byte sourceAddrTon) {
+        this.sourceAddrTon = sourceAddrTon;
+    }
+
+    public byte getSourceAddrNpi() {
+        return sourceAddrNpi;
+    }
+
+    public void setSourceAddrNpi(byte sourceAddrNpi) {
+        this.sourceAddrNpi = sourceAddrNpi;
+    }
+
+    public String getSourceAddr() {
+        return sourceAddr;
+    }
+
+    public void setSourceAddr(String sourceAddr) {
+        this.sourceAddr = sourceAddr;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(final String messageId) {
+        this.messageId = messageId;
+    }
+
+    public byte getPriorityFlag() {
+        return priorityFlag;
+    }
+
+    public void setPriorityFlag(byte priorityFlag) {
+        this.priorityFlag = priorityFlag;
+    }
+
+    public String getScheduleDeliveryTime() {
+        return scheduleDeliveryTime;
+    }
+
+    public void setScheduleDeliveryTime(String scheduleDeliveryTime) {
+        this.scheduleDeliveryTime = scheduleDeliveryTime;
+    }
+
+    public String getValidityPeriod() {
+        return validityPeriod;
+    }
+
+    public void setValidityPeriod(String validityPeriod) {
+        this.validityPeriod = validityPeriod;
+    }
+
+    public byte getReplaceIfPresentFlag() {
+        return replaceIfPresentFlag;
+    }
+
+    public void setReplaceIfPresentFlag(byte replaceIfPresentFlag) {
+        this.replaceIfPresentFlag = replaceIfPresentFlag;
+    }
+
+    public byte getDataCoding() {
+        return dataCoding;
+    }
+
+    public void setDataCoding(byte dataCoding) {
+        this.dataCoding = dataCoding;
+    }
+
+    public byte getSmDefaultMsgId() {
+        return smDefaultMsgId;
+    }
+
+    public void setSmDefaultMsgId(byte smDefaultMsgId) {
+        this.smDefaultMsgId = smDefaultMsgId;
+    }
+
+    public <U extends OptionalParameter> U getOptionalParameter(Class<U> tagClass)
+    {
+    	return OptionalParameters.get(tagClass, optionalParameters);
+    }
+    
+    public OptionalParameter getOptionalParameter(Tag tagEnum)
+    {
+    	return OptionalParameters.get(tagEnum.code(), optionalParameters);
+    }
+    
+    public OptionalParameter[] getOptionalParameters() {
+        return optionalParameters;
+    }
+
+    public void setOptionalParameters(OptionalParameter[] optionalParameters) {
+        this.optionalParameters = optionalParameters;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (serviceType != null ? serviceType.hashCode() : 0);
+        result = 31 * result + (int) sourceAddrTon;
+        result = 31 * result + (int) sourceAddrNpi;
+        result = 31 * result + (sourceAddr != null ? sourceAddr.hashCode() : 0);
+        result = 31 * result + (messageId != null ? messageId.hashCode() : 0);
+        result = 31 * result + (int) priorityFlag;
+        result = 31 * result + (scheduleDeliveryTime != null ? scheduleDeliveryTime.hashCode() : 0);
+        result = 31 * result + (validityPeriod != null ? validityPeriod.hashCode() : 0);
+        result = 31 * result + (int) replaceIfPresentFlag;
+        result = 31 * result + (int) dataCoding;
+        result = 31 * result + (int) smDefaultMsgId;
+        result = 31 * result + Arrays.hashCode(optionalParameters);
+        return result;
+    }
+
+}
