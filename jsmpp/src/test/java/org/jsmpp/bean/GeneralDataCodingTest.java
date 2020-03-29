9023aa46bd91bf540ad9b2975f77e52bbd0e24a8
Pim Moerenhout <pim.moerenhout@gmail.com>
2020-03-29 13:08:48 +0200

    Add extra tests for GeneralDataCoding.

--- jsmpp/src/test/java/org/jsmpp/bean/GeneralDataCodingTest.java
+++ jsmpp/src/test/java/org/jsmpp/bean/GeneralDataCodingTest.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.jsmpp.bean;
 
 import static org.testng.Assert.assertEquals;
@@ -22,9 +36,63 @@ public class GeneralDataCodingTest {
   }
 
   @Test
-  public void testGeneralAlphabetUcs2() {
+  public void testGeneralAlphabetLatin1() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_LATIN1);
+    assertEquals(dataCoding.toByte(), (byte) 0x03);
+  }
+
+  @Test
+  public void testGeneralAlphabetBinary() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_8_BIT);
+    assertEquals(dataCoding.toByte(), (byte) 0x04);
+  }
+
+  @Test
+  public void testGeneralAlphabetJis() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_JIS);
+    assertEquals(dataCoding.toByte(), (byte) 0x05);
+  }
+
+  @Test
+  public void testGeneralAlphabetCyrillic() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_CYRILLIC);
+    assertEquals(dataCoding.toByte(), (byte) 0x06);
+  }
+
+  @Test
+  public void testGeneralAlphabetLatinHebrew() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_LATIN_HEBREW);
+    assertEquals(dataCoding.toByte(), (byte) 0x07);
+  }
+
+  @Test
+  public void testGeneralAlphabetUCS2() {
     DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_UCS2);
     assertEquals(dataCoding.toByte(), (byte) 0x08);
+  }
+
+  @Test
+  public void testGeneralAlphabetPictogramEncoding() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_PICTOGRAM_ENCODING);
+    assertEquals(dataCoding.toByte(), (byte) 0x09);
+  }
+
+  @Test
+  public void testGeneralAlphabetIso2022JpMusicCodes() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_ISO_2022_JP_MUSIC_CODES);
+    assertEquals(dataCoding.toByte(), (byte) 0x0a);
+  }
+
+  @Test
+  public void testGeneralAlphabetJisX02121990() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_JIS_X_0212_1990);
+    assertEquals(dataCoding.toByte(), (byte) 0x0d);
+  }
+
+  @Test
+  public void testGeneralAlphabetKsC5601() {
+    DataCoding dataCoding = new GeneralDataCoding(Alphabet.ALPHA_KS_C_5601);
+    assertEquals(dataCoding.toByte(), (byte) 0x0e);
   }
 
   @Test
