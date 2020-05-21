8ddfccb6a1c8422e7b5a2c5a42e7e6c28efcacb8
Pim Moerenhout <pim.moerenhout@gmail.com>
2020-05-21 08:08:05 +0200

    Extend the Connection interface to show all local and remote socket addresses and ports.

--- jsmpp/src/main/java/org/jsmpp/session/connection/socket/SocketConnection.java
+++ jsmpp/src/main/java/org/jsmpp/session/connection/socket/SocketConnection.java
@@ -1,16 +1,16 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 package org.jsmpp.session.connection.socket;
 
@@ -42,10 +42,18 @@ public class SocketConnection implements Connection {
         this.out = socket.getOutputStream();
     }
 
+    public SocketConnection(Socket socket, int size) throws IOException {
+        this.socket = socket;
+        this.in = new StrictBufferedInputStream(socket.getInputStream(), size);
+        this.out = socket.getOutputStream();
+    }
+
+    @Override
     public void setSoTimeout(int timeout) throws IOException {
         socket.setSoTimeout(timeout);
     }
 
+    @Override
     public void close() {
         try {
             socket.close();
@@ -54,19 +62,39 @@ public class SocketConnection implements Connection {
         }
     }
 
+    @Override
     public boolean isOpen() {
         return !socket.isClosed();
     }
 
+    @Override
     public InputStream getInputStream() {
         return in;
     }
 
+    @Override
     public OutputStream getOutputStream() {
         return out;
     }
 
+    @Override
     public InetAddress getInetAddress() {
         return socket.getInetAddress();
     }
+
+    @Override
+    public InetAddress getLocalAddress() {
+        return socket.getLocalAddress();
+    }
+
+    @Override
+    public int getPort() {
+        return socket.getPort();
+    }
+
+    @Override
+    public int getLocalPort() {
+        return socket.getLocalPort();
+    }
+
 }
