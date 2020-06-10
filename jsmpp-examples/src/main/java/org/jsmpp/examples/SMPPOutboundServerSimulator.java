3e1818e3b914e466bffea3153c6518956be0baca
Pim Moerenhout <pim.moerenhout@gmail.com>
2020-06-10 21:05:13 +0200

    Improve outbound ESME server example.

--- jsmpp-examples/src/main/java/org/jsmpp/examples/SMPPOutboundServerSimulator.java
+++ jsmpp-examples/src/main/java/org/jsmpp/examples/SMPPOutboundServerSimulator.java
@@ -1,16 +1,16 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 package org.jsmpp.examples;
 
@@ -24,7 +24,6 @@ import org.jsmpp.bean.BindType;
 import org.jsmpp.bean.DeliverSm;
 import org.jsmpp.bean.NumberingPlanIndicator;
 import org.jsmpp.bean.TypeOfNumber;
-import org.jsmpp.extra.ProcessRequestException;
 import org.jsmpp.extra.SessionState;
 import org.jsmpp.session.BindParameter;
 import org.jsmpp.session.OutbindRequest;
@@ -38,15 +37,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
  * @author pmoerenhout
- *
  */
 public class SMPPOutboundServerSimulator extends ServerResponseDeliveryAdapter
     implements Runnable, OutboundServerMessageReceiverListener {
   private static final Logger LOGGER = LoggerFactory.getLogger(SMPPOutboundServerSimulator.class);
   private static final Integer DEFAULT_PORT = 8056;
-  private final ExecutorService execService = Executors.newFixedThreadPool(100);
+  private final ExecutorService execService = Executors.newFixedThreadPool(10);
   private int port;
 
   private AtomicBoolean exit = new AtomicBoolean();
@@ -59,13 +56,11 @@ public class SMPPOutboundServerSimulator extends ServerResponseDeliveryAdapter
     int port;
     try {
       port = Integer.parseInt(System.getProperty("jsmpp.simulator.port", DEFAULT_PORT.toString()));
-    }
-    catch (NumberFormatException e) {
+    } catch (NumberFormatException e) {
       port = DEFAULT_PORT;
     }
 
     SMPPOutboundServerSimulator smppServerSim = new SMPPOutboundServerSimulator(port);
-    LOGGER.info("run {}", smppServerSim);
     smppServerSim.run();
   }
 
@@ -75,7 +70,7 @@ public class SMPPOutboundServerSimulator extends ServerResponseDeliveryAdapter
 
   public void run() {
     try {
-      LOGGER.info("Listening on port {}", port);
+      LOGGER.info("SMPP Outbound server listening on port {}", port);
       OutboundSMPPServerSessionListener sessionListener = new OutboundSMPPServerSessionListener(port);
 
       while (!exit.get()) {
@@ -86,29 +81,19 @@ public class SMPPOutboundServerSimulator extends ServerResponseDeliveryAdapter
         outboundServerSession.addSessionStateListener(new SessionStateListenerImpl());
         outboundServerSession.setOutboundServerMessageReceiverListener(this);
 
-        execService.execute(new SMPPOutboundServerSimulator.WaitOutbindTask(outboundServerSession));
-
-        try {
-          Thread.sleep(60 * 60 * 1000L);
-        }
-        catch (InterruptedException e) {
-          LOGGER.info("Thread was interrupted");
-          shutdown();
-        }
-        outboundServerSession.close();
+        execService.submit(new SMPPOutboundServerSimulator.WaitOutbindTask(outboundServerSession));
       }
 
-      LOGGER.info("close listener {}", sessionListener);
+      LOGGER.info("Close listener {}", sessionListener);
       sessionListener.close();
+    } catch (IOException e) {
+      LOGGER.error("I/O error occurred", e);
+    } finally {
       execService.shutdown();
-    }
-    catch (IOException e) {
-      LOGGER.error("IO error occurred", e);
     }
   }
 
-  public void onAcceptDeliverSm(DeliverSm deliverSm, SMPPOutboundServerSession source)
-      throws ProcessRequestException {
+  public void onAcceptDeliverSm(DeliverSm deliverSm, SMPPOutboundServerSession source) {
     LOGGER.info("deliver_sm: {} {} => {} {}", deliverSm.getSequenceNumber(), deliverSm.getSourceAddr(),
         deliverSm.getDestAddress(), new String(deliverSm.getShortMessage()));
   }
@@ -128,23 +113,20 @@ public class SMPPOutboundServerSimulator extends ServerResponseDeliveryAdapter
             outbindRequest.getSystemId(), outbindRequest.getPassword());
 
         serverSession.bind(new BindParameter(BindType.BIND_TRX, "test", "test", "cp", TypeOfNumber.UNKNOWN,
-            NumberingPlanIndicator.UNKNOWN, null), 60000);
-      }
-      catch (IllegalStateException e) {
+            NumberingPlanIndicator.UNKNOWN, null), 15000);
+      } catch (IllegalStateException e) {
         LOGGER.error("System error", e);
-      }
-      catch (TimeoutException e) {
+      } catch (TimeoutException e) {
         LOGGER.warn("Wait for outbind has reached timeout", e);
-      }
-      catch (IOException e) {
-        LOGGER.warn("IO exception", e);
+      } catch (IOException e) {
+        LOGGER.warn("I/O error occurred", e);
       }
     }
   }
 
   private class SessionStateListenerImpl implements SessionStateListener {
     public void onStateChange(SessionState newState, SessionState oldState, Session source) {
-      LOGGER.info("Session state changed from " + oldState + " to " + newState);
+      LOGGER.info("Session {} state changed from {} to {}", source.getSessionId(), oldState, newState);
     }
   }
 
