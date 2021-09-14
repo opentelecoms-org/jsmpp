33d154befa65eb4c007e467ab102ce6dd18b14d6
Pim Moerenhout <pim.moerenhout@gmail.com>
2021-09-14 08:18:41 +0200

    Add SMPP 3.3 clearification.

--- jsmpp/src/main/java/org/jsmpp/bean/BindType.java
+++ jsmpp/src/main/java/org/jsmpp/bean/BindType.java
@@ -1,29 +1,31 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 package org.jsmpp.bean;
 
 import org.jsmpp.SMPPConstant;
 
 /**
- * Enum represent bind type of SMPP. There are 3 bind types provided by SMPP:
+ * Enum represent bind type of SMPP. There are 3 bind types provided by SMPP 3.4:
  *
  * <ul>
  * <li>receiver (BIND_RX) - receive only</li>
  * <li>transmitter (BIND_TX) - transmit only</li>
  * <li>transceiver (BIND_TRX) - receive and transmit</li>
  * </ul>
+ *
+ * For SMPP 3.3, only BIND_TX and BIND_RX are defined.
  * 
  * @author uudashr
  * @version 1.0
@@ -46,9 +48,9 @@ public enum BindType {
      */
     BIND_TRX(SMPPConstant.CID_BIND_TRANSCEIVER);
 
-    private int bindCommandId;
+    private final int bindCommandId;
 
-    private BindType(int bindCommandId) {
+    BindType(int bindCommandId) {
         this.bindCommandId = bindCommandId;
     }
 
@@ -62,7 +64,7 @@ public enum BindType {
     }
 
     /**
-     * Return the response command_id that should given by this bind type.
+     * Return the response command_id that should be given by this bind type.
      * 
      * @return the bind response command_id.
      */
@@ -97,7 +99,7 @@ public enum BindType {
      * @throws IllegalArgumentException if there is no {@code BindType}
      *         associated with specified {@code bindCommandId} found.
      */
-    public static final BindType valueOf(int bindCommandId)
+    public static BindType valueOf(int bindCommandId)
             throws IllegalArgumentException {
         for (BindType bindType : values()) {
             if (bindType.bindCommandId == bindCommandId) {
