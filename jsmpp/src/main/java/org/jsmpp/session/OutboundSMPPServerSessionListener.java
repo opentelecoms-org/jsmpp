f896d0339acf71e0ac0fe733efe164dbce6404d0
Pim Moerenhout <pim.moerenhout@gmail.com>
2021-09-15 17:54:23 +0200

    Reformat source file

--- jsmpp/src/main/java/org/jsmpp/session/OutboundSMPPServerSessionListener.java
+++ jsmpp/src/main/java/org/jsmpp/session/OutboundSMPPServerSessionListener.java
@@ -1,16 +1,16 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 package org.jsmpp.session;
 
@@ -25,27 +25,27 @@ import org.jsmpp.session.connection.socket.ServerSocketConnectionFactory;
 /**
  * This object responsible to for new SMPP Session request from ESME. It will
  * listen on specified port.
- * 
+ *
  * <pre>
  * SMPPServerSession session = listener.accept();
  * BindRequest bindReq = session.waitForBind(5000);
- * 
+ *
  * if (checkPassword(bindReq)) {
  *     bindReq.accept(&quot;sys&quot;);
  * } else {
  *     bindReq.reject(SMPPConstant.STAT_ESME_RINVPASWD);
  * }
  * </pre>
- * 
+ *
  * <p>
  * The listening trough getting the bind request should take less than session
  * initiation timer, otherwise if there is network open has been requested, ESME
  * will close the connection. Accepting the bind request should take less than
- * transaction timer or ESME will issued timeout.
+ * transaction timer or ESME will issue a timeout.
  *
- * 
+ *
  * @author uudashr
- * 
+ *
  */
 public class OutboundSMPPServerSessionListener {
     private final int port;
@@ -55,81 +55,78 @@ public class OutboundSMPPServerSessionListener {
     private SessionStateListener sessionStateListener;
     private GenericMessageReceiverListener messageReceiverListener;
     private OutboundServerMessageReceiverListener outboundServerMessageReceiverListener;
-    //private ServerResponseDeliveryListener responseDeliveryListener;
 
     public OutboundSMPPServerSessionListener(int port) throws IOException {
         this(port, new ServerSocketConnectionFactory());
     }
 
-    public OutboundSMPPServerSessionListener(int port,
-                                             ServerConnectionFactory serverConnFactory) throws IOException {
+    public OutboundSMPPServerSessionListener(int port, ServerConnectionFactory serverConnFactory)
+        throws IOException {
         this.port = port;
         serverConn = serverConnFactory.listen(port);
     }
 
-    public OutboundSMPPServerSessionListener(int port, int timeout,
-                                             ServerConnectionFactory serverConnFactory) throws IOException {
+    public OutboundSMPPServerSessionListener(int port, int timeout, ServerConnectionFactory serverConnFactory)
+        throws IOException {
         this.port = port;
         serverConn = serverConnFactory.listen(port, timeout);
     }
 
-    public OutboundSMPPServerSessionListener(int port, int timeout, int backlog,
-                                             ServerConnectionFactory serverConnFactory) throws IOException {
+    public OutboundSMPPServerSessionListener(int port, int timeout, int backlog, ServerConnectionFactory serverConnFactory)
+        throws IOException {
         this.port = port;
         serverConn = serverConnFactory.listen(port, timeout, backlog);
     }
-    
+
     public int getTimeout() throws IOException {
         return serverConn.getSoTimeout();
     }
-    
+
     /**
      * Timeout listening. When timeout reach and connection request didn't
      * arrive then {@link SocketTimeoutException} will be thrown but the
      * listener still valid.
-     * 
+     *
      * @param timeout the timeout in milliseconds
      * @throws IOException if an input or output error occurs.
      */
     public void setTimeout(int timeout) throws IOException {
         serverConn.setSoTimeout(timeout);
     }
-    
+
     public void setPduProcessorDegree(int pduProcessorDegree) {
         this.pduProcessorDegree = pduProcessorDegree;
     }
-    
+
     public int getPduProcessorDegree() {
         return pduProcessorDegree;
     }
-    
+
     public int getPort() {
         return port;
     }
-    
+
     public int getInitiationTimer() {
         return initiationTimer;
     }
-    
+
     public void setInitiationTimer(int initiationTimer) {
         this.initiationTimer = initiationTimer;
     }
-    
+
     public SessionStateListener getSessionStateListener() {
         return sessionStateListener;
     }
-    
-    public void setSessionStateListener(
-            SessionStateListener sessionStateListener) {
+
+    public void setSessionStateListener(SessionStateListener sessionStateListener) {
         this.sessionStateListener = sessionStateListener;
     }
-    
+
     public GenericMessageReceiverListener getMessageReceiverListener() {
         return messageReceiverListener;
     }
-    
-    public void setMessageReceiverListener(
-        GenericMessageReceiverListener messageReceiverListener) {
+
+    public void setMessageReceiverListener(GenericMessageReceiverListener messageReceiverListener) {
         this.messageReceiverListener = messageReceiverListener;
     }
 
@@ -137,31 +134,25 @@ public class OutboundSMPPServerSessionListener {
         return outboundServerMessageReceiverListener;
     }
 
-    public void setOutboundServerMessageReceiverListener(
-        OutboundServerMessageReceiverListener outboundServerMessageReceiverListener) {
+    public void setOutboundServerMessageReceiverListener(OutboundServerMessageReceiverListener outboundServerMessageReceiverListener) {
         this.outboundServerMessageReceiverListener = outboundServerMessageReceiverListener;
     }
-    
-//    public void setResponseDeliveryListener(
-//            ServerResponseDeliveryListener responseDeliveryListener) {
-//        this.responseDeliveryListener = responseDeliveryListener;
-//    }
-    
+
     /**
      * Accept session request from client. The session state is still OPEN. To
      * communicate with ESME properly binding request should be accepted.
-     * 
+     *
      * <pre>
      * SMPPServerSession session = listener.accept();
      * BindRequest bindReq = session.waitForBind(5000);
-     * 
+     *
      * if (checkPassword(bindReq)) {
      *     bindReq.accept(&quot;sys&quot;);
      * } else {
      *     bindReq.reject(SMPPConstant.STAT_ESME_RINVPASWD);
      * }
      * </pre>
-     * 
+     *
      * @return the accepted {@link SMPPOutboundServerSession}.
      * @throws SocketTimeoutException if timeout reach with no session accepted.
      * @throws IOException if there is an input or output error.
@@ -175,7 +166,7 @@ public class OutboundSMPPServerSessionListener {
                 messageReceiverListener, outboundServerMessageReceiverListener,
                 pduProcessorDegree);
     }
-    
+
     public void close() throws IOException {
         serverConn.close();
     }
